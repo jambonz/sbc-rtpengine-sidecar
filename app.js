003f32058996f763d8cf55fd01b9a6ffce6a9cad
Quan HL <quan.luuhoang8@gmail.com>
2023-11-10 11:11:19 +0700

    let realtimedb-helper build configuration from env vars

--- app.js
+++ app.js
@@ -39,31 +39,7 @@ if (!noSip) {
       assert.ok(process.env.JAMBONES_REDIS_HOST, 'missing JAMBONES_REDIS_HOST env var');
     }
 
-    const JAMBONES_REDIS_SENTINELS = process.env.JAMBONES_REDIS_SENTINELS ? {
-      sentinels: process.env.JAMBONES_REDIS_SENTINELS.split(',').map((sentinel) => {
-        let host, port = 26379;
-        if (sentinel.includes(':')) {
-          const arr = sentinel.split(':');
-          host = arr[0];
-          port = parseInt(arr[1], 10);
-        } else {
-          host = sentinel;
-        }
-        return {host, port};
-      }),
-      name: process.env.JAMBONES_REDIS_SENTINEL_MASTER_NAME,
-      ...(process.env.JAMBONES_REDIS_SENTINEL_PASSWORD && {
-        password: process.env.JAMBONES_REDIS_SENTINEL_PASSWORD
-      }),
-      ...(process.env.JAMBONES_REDIS_SENTINEL_USERNAME && {
-        username: process.env.JAMBONES_REDIS_SENTINEL_USERNAME
-      })
-    } : null;
-
-    const {monitorSet, removeFromSet} = require('@jambonz/realtimedb-helpers')(JAMBONES_REDIS_SENTINELS || {
-      host: process.env.JAMBONES_REDIS_HOST,
-      port: process.env.JAMBONES_REDIS_PORT || 6379
-    }, logger);
+    const {monitorSet, removeFromSet} = require('@jambonz/realtimedb-helpers')({}, logger);
     srf.locals = {...srf.locals,
       stats,
       dbHelpers: {
